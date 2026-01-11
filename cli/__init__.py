605d85dfde66fd1ac3907d9be8c1504dd2f0b9f7
Gyreedy <uskviral@gmail.com>
2026-01-11 19:46:16 +0000

    expose CLI entrypoint via cli package

---
+++ cli/__init__.py
@@ -0,0 +1,11 @@
+"""
+YAWN CLI package.
+
+This module defines the public command-line interface entrypoints
+for interacting with the YAWN execution framework.
+"""
+
+from .main import main
+
+__all__ = ["main"]
+
